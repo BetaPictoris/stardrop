46281c59e837ade394f5be4dd5f53164a5d52ae2
BetaPictoris <beta@ozx.me>
2022-12-20 23:40:51 -0800

    Added categories map for songs

---
+++ src/components/AudioController/data.js
@@ -0,0 +1,88 @@
+const cdnBaseURL = "https://cdn.ozx.me";
+
+const categories = {
+  Games: {
+    JourneyOfThePrairieKing: ["FinalBossAndEnding", "Overworld", "TheOutlaw"],
+  },
+
+  NPCThemes: {
+    Normal: [
+      "Abigail",
+      "Alex",
+      "AlexKeepsake",
+      "DrHarvey",
+      "Elliott",
+      "Grandpa",
+      "Haley",
+      "Leah",
+      "Maru",
+      "Penny",
+      "Sebastian",
+      "Wizard",
+    ],
+    SamsBand: ["Bluegrass", "Electronic", "Heavy", "Pop", "Practice"],
+  },
+
+  Other: {
+    Events: [
+      "Fair",
+      "FestivalGame",
+      "FlowerDance",
+      "FunFestival",
+      "GoldenStar",
+      "LuauFestival",
+      "MoonlightJellies",
+      "SpiritsEve",
+      "Wedding",
+      "WinterFestival",
+    ],
+
+    Normal: [
+      "ButtercupMelody",
+      "CloudCountry",
+      "DistantBanjo",
+      "Jaunty",
+      "Load",
+      "MusicBox",
+      "Overture",
+      "Playful",
+      "SettlingIn",
+      "Violin",
+    ],
+  },
+
+  Places: {
+    Mines: [
+      "AFlickerInTheDeep",
+      "Cloth",
+      "CrystalBells",
+      "Danger",
+      "Icicles",
+      "MagicalShoes",
+      "MarimbaOfFrozenBones",
+      "StarLumpy",
+      "TheLavaDwellers",
+      "VisitorToTheUnknown",
+    ],
+
+    Normal: [
+      "ADarkCornerOfThePast",
+      "CalicoDesert",
+      "CountryShop",
+      "InTheDeepWoods",
+      "PelicanTown",
+      "TheAdventureGuild",
+      "TheLibraryAndMuseum",
+      "TheStardropSaloon",
+    ],
+  },
+
+  Seasons: {
+    Fall: ["GhostSynth", "RavensDescent", "TheSmellOfMushroom"],
+    Spring: [
+      "ItsABigWorldOutside",
+      "TheValleyComesAlive",
+      "WildHorseradishJam",
+    ],
+  },
+};
